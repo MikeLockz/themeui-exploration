6a52db0f1e1b47b36eb84d403336c6bb4c4f0656
Mike Lock <michael.lockwitz@consensys.net>
2020-02-27 16:17:53 -0700

    Add example codefi theme

--- src/App.js
+++ src/App.js
@@ -1,78 +1,13 @@
 /** @jsx jsx */
 import { jsx, Box, Button, ThemeProvider } from "theme-ui";
+import CodefiTheme from "./theme";
 
-// These are the defaultProps that are used for Codefi
-// Button.defaultProps = {
-//   position: 'relative',
-
-//   mainColor: 'primary',
-//   contrastColor: 'white',
-
-//   height: 'auto',
-//   px: 6,
-//   py: 3,
-//   border: 'none',
-//   borderRadius: '4px',
-
-//   lineHeight: '24px',
-//   fontWeight: '600',
-// }
-
-const theme = {
-  colors: {
-    text: "#000",
-    background: "#fff",
-    primary: "#1A5AFE",
-    black: "#000",
-    white: "#fff",
-    blacks: [
-      "rgba(0,0,0,.0125)",
-      "rgba(0,0,0,.025)",
-      "rgba(0,0,0,.05)",
-      "rgba(0,0,0,.1)",
-      "rgba(0,0,0,.2)",
-      "rgba(0,0,0,.3)",
-      "rgba(0,0,0,.4)",
-      "rgba(0,0,0,.5)",
-      "rgba(0,0,0,.6)",
-      "rgba(0,0,0,.7)",
-      "rgba(0,0,0,.8)",
-      "rgba(0,0,0,.9)"
-    ],
-    whites: [
-      "rgba(255,255,255,.0125)",
-      "rgba(255,255,255,.025)",
-      "rgba(255,255,255,.05)",
-      "rgba(255,255,255,.1)",
-      "rgba(255,255,255,.2)",
-      "rgba(255,255,255,.3)",
-      "rgba(255,255,255,.4)",
-      "rgba(255,255,255,.5)",
-      "rgba(255,255,255,.6)",
-      "rgba(255,255,255,.7)",
-      "rgba(255,255,255,.8)",
-      "rgba(255,255,255,.9)"
-    ]
-  },
-  space: [0, 4, 8, 12, 16, 20, 24, 32, 40],
-  fontSizes: [12, 14, 16, 20, 24, 32, 48, 64],
-  fontWeights: {
-    body: 400,
-    heading: 500
-  },
-  lineHeights: {
-    body: 1.5,
-    heading: 1
-  },
-  fonts: {
-    // serif: 'athelas, georgia, times, serif',
-    sansSerif: '"Inter", "Helvetica", Arial, sans-serif',
-    mono: 'Monaco, Consolas, "Lucida Console", monospace'
-  },
+const customCodefiTheme = {
+  ...CodefiTheme,
   buttons: {
     primary: {
       position: "relative",
-      bg: "primary",
+      bg: "primary700",
       color: "white",
 
       height: "auto",
@@ -85,17 +20,25 @@ const theme = {
       fontWeight: "bold",
 
       "&:hover": {
-        bg: "text"
+        bg: "primary800"
+      },
+      "&:active": {
+        bg: "primary900"
+      },
+      "&:disabled": {
+        bg: "primary400"
       }
     }
   }
 };
 
 function App() {
+  console.log("Theme", CodefiTheme);
   return (
-    <ThemeProvider theme={theme} className={"app"}>
+    <ThemeProvider theme={customCodefiTheme} className={"app"}>
       <Box p={8} sx={{ mb: 4 }} width="400px">
         <Button>I am a styled from the theme!</Button>
+        <Button disabled>Disabled :-(</Button>
       </Box>
     </ThemeProvider>
   );
